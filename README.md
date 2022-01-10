1e8168518032d861e0afa58dbfb219fc1bfe65ea
mg <info@jdotsoft.com>
2022-01-09 23:07:21 -0500

    Initial check-in

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# SvnLatestRevisionMavenPlugin
+# SVN Latest Revision Maven Plugin
+Simple SVN Latest Revision Maven Plugin.
+
+The **SVN Latest Revision Plugin** is used to retrieve the project latest SVN revision information from the project
+`.svn` folder and store it in a Maven properties. The plugin has only one goal `svn-latest-revision:run`
+executed in Maven `INITIALIZE` stage. The goal opens SVN SQLite database file in `.svn` project directory
+and queries it for the latest revision.
+
+**NOTE:** the SVN latest revision reflects the latest checked-in however the project build may have not committed changes.
+
+After this plugin goal execution the latest revision and its timestamp are stored in the Maven properties:
+- `svn.latest.revision`
+- `svn.latest.timestamp`
+
+See also [http://www.jdotsoft.com/SvnMavenPlugin.php](http://www.jdotsoft.com/SvnMavenPlugin.php)
+
+The plugin declaration in pom-file:
+
+    <plugin>
+      <groupId>com.jdotsoft</groupId>
+      <artifactId>svn-latest-revision-maven-plugin</artifactId>
+      <version>1.0</version>
+      <executions>
+        <execution>
+          <goals>
+            <goal>run</goal>
+          </goals>
+        </execution>
+      </executions>
+      <configuration>
+        <timestampFormat>dd-MMM-yyyy HH:ss</timestampFormat>
+      </configuration>
+    </plugin>
+
+The configuration `timestampFormat` is optional, the default value is `dd-MMM-yyyy HH:ss`
+
+Example plugin console output:
+
+    [INFO] --- svn-latest-revision-maven-plugin:1.0:run (default) @ MyProject ---
+    [INFO] Opening SQLite SVN database file C:\projects\MyProject\.svn\wc.db
+    [INFO] Connected to DB. Latest revision:
+    [INFO]   1929 18-Dec-2021 20:09 | MyProject/src/main/java/com/jdotsoft/demo/Abc.java
+    [INFO]   1929 18-Dec-2021 20:09 | MyProject/src/main/java/com/jdotsoft/demo/Xyz.java
+
+Maven properties defined by this plugin execution could be used, for example, in a `MANIFEST.MF` file:
+
+    <plugin>
+      <groupId>org.apache.maven.plugins</groupId>
+      <artifactId>maven-jar-plugin</artifactId>
+      . . .
+      <configuration>
+        <archive>
+          <manifestEntries>
+            <Build-Time>${local.build.timestamp}</Build-Time>
+            <SVN-latest-revision>${svn.latest.revision} ${svn.latest.timestamp}</SVN-latest-revision>
+          </manifestEntries>
+        </archive>
+      </configuration>
+    </plugin>
+
+### Other features
+Maven has a build-in property `maven.build.timestamp` which could be formatted with specified in a POM-file pattern:
+
+    <properties>
+      <maven.build.timestamp.format>dd-MMM-yyyy HH:mm</maven.build.timestamp.format>
+    </properties>
+
+Maven uses UTC timezone for build timestamp property `maven.build.timestamp`
+while SVN latest revision timestamp is in a local timezone.
+The time zone discrepancy could lead to a confusion if both timestamps are displayed to a user.
+
+The **SVN Latest Revision Plugin** creates Maven property `local.build.timestamp` similar to `maven.build.timestamp`
+reusing the Maven timestamp format `maven.build.timestamp.format`.
+The property `local.build.timestamp` could be used as a `maven.build.timestamp`
+replacement to make both timestamps in local time zone (see example above).
---
+++ src/main/java/com/jdotsoft/svnrevision/SvnLatestRevisionMojo.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2022 JDotSoft. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jdotsoft.svnrevision;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Provide latest project SVN revision as a Maven property.
+ */
+@Mojo(name="run", defaultPhase=LifecyclePhase.INITIALIZE)
+public class SvnLatestRevisionMojo extends AbstractMojo {
+  private static final String PROPERTY_MAVEN_BUILD_TIMESTAMP_FORMAT = "maven.build.timestamp.format";
+  private static final String PROPERTY_LOCAL_BUILD_TIMESTAMP = "local.build.timestamp";
+  private static final String PROPERTY_SVN_LATEST_REVISION = "svn.latest.revision";
+  private static final String PROPERTY_SVN_LATEST_TIMESTAMP = "svn.latest.timestamp";
+  private static final String SQL = "SELECT * FROM nodes WHERE revision = (SELECT MAX(revision) FROM nodes)";
+  private static final String DB_COLUMN_REVISION = "revision";
+  private static final String DB_COLUMN_CHANGED_DATE = "changed_date";
+  private static final String DB_COLUMN_REPOS_PATH = "repos_path";
+
+  @Parameter(defaultValue="${project}", required=true, readonly=true)
+  private MavenProject project;
+
+  @Parameter(defaultValue="yyyy-dd-MMM zzz", required=false, readonly=true)
+  private String timestampFormat;
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    String fileDB = project.getBasedir() + File.separator + ".svn" + File.separator + "wc.db";
+    Log logger = getLog();
+    logger.info("Opening SQLite SVN database file " + fileDB);
+    SimpleDateFormat fmt = new SimpleDateFormat(timestampFormat);
+    Properties prop = project.getProperties();
+    prop.setProperty(PROPERTY_LOCAL_BUILD_TIMESTAMP,
+        new SimpleDateFormat(prop.getProperty(PROPERTY_MAVEN_BUILD_TIMESTAMP_FORMAT)).format(new Date()));
+    prop.setProperty(PROPERTY_SVN_LATEST_REVISION, "n/a"); // default - in case of exception
+    prop.setProperty(PROPERTY_SVN_LATEST_TIMESTAMP, "");
+    try (
+      Connection con = DriverManager.getConnection("jdbc:sqlite:" + fileDB);
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery(SQL))
+    {
+      logger.info("Connected to DB. Latest revision:");
+      while (rs.next()) {
+        // Revision and timestamp are the same for all SQL query records: the last record is effective
+        int nRevision = rs.getInt(DB_COLUMN_REVISION);
+        String sTimestamp = fmt.format(new Date(rs.getLong(DB_COLUMN_CHANGED_DATE) / 1000)); // check-in UNIX timestamp
+        prop.setProperty(PROPERTY_SVN_LATEST_REVISION, Integer.toString(nRevision));
+        prop.setProperty(PROPERTY_SVN_LATEST_TIMESTAMP, sTimestamp);
+        logger.info(String.format("  %d %s | %s", nRevision, sTimestamp, rs.getString(DB_COLUMN_REPOS_PATH)));
+      }
+    } catch (SQLException e) {
+      logger.error("Failed to query DB", e);
+    }
+  }
+
+}
